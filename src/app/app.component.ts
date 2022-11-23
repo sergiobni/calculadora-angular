8f24a7ca80924fb6d4147e813817078d77e4acc1
unknown <bni.sergio@gmail.com>
2022-11-23 19:20:19 +0100

    falta añadir funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'calculadora-angular';
+  pantalla = '';
+  memoria = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  recoge(a: any) {
+    //   if (typeof a === 'number'){this.memoria += a;}else if{ typeof a === 'string'{
+    //     switch(a){
+    //       case a === '+':
+    //   }}
+  }
 }
